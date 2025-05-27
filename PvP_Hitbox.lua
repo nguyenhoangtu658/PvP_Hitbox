3347f68979ff1e940e513dfb0c06e76093360a76
nguyenhoangtu658 <ca0360056@gmail.com>
2025-05-27 13:08:02 +0700

    Update PvP_Hitbox.lua

--- PvP_Hitbox.lua
+++ PvP_Hitbox.lua
@@ -1,15 +1,25 @@
-spawn(function()
-    while wait(1) do
-        for _, player in pairs(game.Players:GetPlayers()) do
-            if player ~= game.Players.LocalPlayer and player.Character and player.Character:FindFirstChild("HumanoidRootPart") then
-                pcall(function()
-                    local hrp = player.Character.HumanoidRootPart
-                    hrp.Size = Vector3.new(300, 300, 300)
-                    hrp.Transparency = 0.7
-                    hrp.Material = Enum.Material.ForceField
-                    hrp.CanCollide = false
-                end)
-            end
+for _, player in pairs(game:GetService("Players"):GetPlayers()) do
+    if player ~= game.Players.LocalPlayer and player.Character and player.Character:FindFirstChild("HumanoidRootPart") then
+        local hrp = player.Character:FindFirstChild("HumanoidRootPart")
+        hrp.Size = Vector3.new(40, 40, 40)
+        hrp.Transparency = 1
+        hrp.CanCollide = false
+        hrp.Massless = true
+        hrp.Anchored = false
+    end
+end
+
+while true do
+    task.wait(0.2)
+    for _, player in pairs(game:GetService("Players"):GetPlayers()) do
+        if player ~= game.Players.LocalPlayer and player.Character and player.Character:FindFirstChild("HumanoidRootPart") then
+            local hrp = player.Character:FindFirstChild("HumanoidRootPart")
+            pcall(function()
+                hrp.Size = Vector3.new(300, 300, 300)
+                hrp.Transparency = 1
+                hrp.CanCollide = false
+                hrp.Massless = true
+            end)
         end
     end
-end)
+end
